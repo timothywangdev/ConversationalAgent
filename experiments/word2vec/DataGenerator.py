40453f140ace602a7dc1fd3a9b8ed06ce631bd3d
Hujie Wang <wanghujie@gmail.com>
2015-08-19 20:08:09 -0400

    Added getData, ConcatJSONDecoder

--- experiments/word2vec/DataGenerator.py
+++ experiments/word2vec/DataGenerator.py
@@ -1,10 +1,36 @@
 __author__ = 'Hujie Wang'
 
 import json
+import re
+
+
+FLAGS = re.VERBOSE | re.MULTILINE | re.DOTALL
+WHITESPACE = re.compile(r'[ \t\n\r]*', FLAGS)
+
+class ConcatJSONDecoder(json.JSONDecoder):
+    def decode(self, s, _w=WHITESPACE.match):
+        s_len = len(s)
+
+        objs = []
+        end = 0
+        while end != s_len:
+            obj, end = self.raw_decode(s, idx=_w(s, end).end())
+            end = _w(s, end).end()
+            objs.append(obj)
+        return objs
 
 class DataGenerator(object):
     def __init__(self,data_path):
         self.data_path=data_path
+
+    def getData(self,fname):
+        '''
+        :param fname(Reddit comment JSON file):
+        :return a dict of JSON object:
+        '''
+        with open('./data/data.json', encoding='utf-8') as data_file:
+            data = json.loads(data_file.read(),cls=ConcatJSONDecoder)
+        return data
 
     def getSentences(self,data):
         '''
